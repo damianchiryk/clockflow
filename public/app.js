7479b7473c188fd010e89d74ba9960ca3cbb5784
Damian <damianchiryk@gmail.com>
2026-04-04 08:49:24 +0100

    fix public files

--- public/app.js
+++ public/app.js
@@ -1,11 +1,13 @@
+function clock(action){
+  const name = document.getElementById('user').value;
+  const pin = document.getElementById('pin').value;
 
-function clock(action) {
-  const name = document.getElementById('name').value;
-
-  fetch('/clock', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ name, action })
+  fetch('/clock',{
+    method:'POST',
+    headers:{'Content-Type':'application/json'},
+    body: JSON.stringify({name, action, pin})
   })
-  .then(() => alert('Saved'));
+  .then(()=> {
+    document.getElementById('msg').innerText = "Saved";
+  });
 }
